1509cb1cd1309258730dff0b1823f8fc77f54dde
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-14 20:00:04 +0100

    8 listo

--- Ejercicio8_pares/js/pares.js
+++ Ejercicio8_pares/js/pares.js
@@ -1,9 +1,10 @@
 var num;
+
 function calcula(valor){
     num = valor.value;
     if(num % 2 == 0) {
-      document.getElementById("result").innerHTML += "es un número par";
+      document.getElementById("result").innerHTML += " es un número par";
     } else {
-       document.getElementById("result").innerHTML += "es un número impar";
+       document.getElementById("result").innerHTML += " es un número impar";
     }
 }
